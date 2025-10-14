255ce4cb51de90771bf29bfdf2defaee210db06e
Sivert Schou Olsen <sivertschou@hotmail.com>
2025-10-14 13:08:35 +0200

    Add biome

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -15,6 +15,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
+    { import = "lazyvim.plugins.extras.formatting.biome" },
     -- import/override with your plugins
     { import = "lazyvim.plugins.extras.editor.telescope" },
     { import = "plugins" },
