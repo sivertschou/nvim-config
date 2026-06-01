cccf77ce45c49539ad047c93d66354db37e5ef37
Sivert Schou Olsen <sivertschou@hotmail.com>
2026-06-01 09:52:25 +0200

    Update plugins

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -12,10 +12,10 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.typescript.biome" },
     { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
-    { import = "lazyvim.plugins.extras.formatting.biome" },
     -- import/override with your plugins
     { import = "lazyvim.plugins.extras.editor.telescope" },
     { import = "plugins" },
